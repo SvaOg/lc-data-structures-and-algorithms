e50935c2d0b724936c578d1e231564c7582ddd66
Alexander Serdyukov <alexander.serdyukov@gmail.com>
2025-09-13 16:48:25 -0600

    Problem 217

---
+++ problems/hasing/problem_217.py
@@ -0,0 +1,48 @@
+"""
+217. Contains Duplicate
+https://leetcode.com/problems/contains-duplicate/
+
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+Example 1:
+Input: nums = [1,2,3,1]
+Output: true
+
+Example 2:
+Input: nums = [1,2,3,4]
+Output: false
+
+Example 3:
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+
+Constraints:
+- 1 <= nums.length <= 10^5
+- -10^9 <= nums[i] <= 10^9
+"""
+
+from typing import List
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return len(nums) > len(set(nums))
+
+import pytest
+
+
+@pytest.fixture
+def solution():
+    return Solution()
+
+
+def test_example_1(solution):
+    assert solution.containsDuplicate([1, 2, 3, 1]) == True
+
+
+def test_example_2(solution):
+    assert solution.containsDuplicate([1, 2, 3, 4]) == False
+
+
+def test_example_3(solution):
+    assert solution.containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]) == True
