670835ca6d42f012efcb59b5d88c2d1a79beac29
Alexander Serdyukov <alexander.serdyukov@gmail.com>
2025-10-05 12:26:22 -0600

    Problem 155

---
+++ problems/stacks_and_queues/problem_155.py
@@ -0,0 +1,79 @@
+"""
+LeetCode Problem 155: Min Stack
+
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+Implement the MinStack class:
+
+- MinStack() initializes the stack object.
+- void push(int val) pushes the element val onto the stack.
+- void pop() removes the element on the top of the stack.
+- int top() gets the top element of the stack.
+- int getMin() retrieves the minimum element in the stack.
+
+You must implement the functions of the class such that each operation works in constant time.
+
+Example 1:
+
+Input
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+Output
+[null,null,null,null,-3,null,0,-2]
+
+Explanation
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin(); // return -3
+minStack.pop();
+minStack.top();    // return 0
+minStack.getMin(); // return -2
+
+Constraints:
+- -2^31 <= val <= 2^31 - 1
+- Methods pop, top and getMin operations will always be called on non-empty stacks.
+- At most 3 * 10^4 calls will be made to push, pop, top, and getMin.
+"""
+
+
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        curr_min = val if not self.stack else min(self.stack[-1][1], val)
+        self.stack.append((val, curr_min))
+
+    def pop(self) -> None:
+        return self.stack.pop()[0]
+
+    def top(self) -> int:
+        return self.stack[-1][0]
+
+    def getMin(self) -> int:
+        return self.stack[-1][1]
+
+
+import pytest
+
+
+def test_example_1():
+    """
+    Example 1:
+    Input
+    ["MinStack","push","push","push","getMin","pop","top","getMin"]
+    [[],[-2],[0],[-3],[],[],[],[]]
+    Output
+    [null,null,null,null,-3,null,0,-2]
+    """
+    minStack = MinStack()
+    minStack.push(-2)
+    minStack.push(0)
+    minStack.push(-3)
+    assert minStack.getMin() == -3
+    minStack.pop()
+    assert minStack.top() == 0
+    assert minStack.getMin() == -2
