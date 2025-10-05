a97fac7504aa308e8ee1fea31991fbed4becb2c8
Alexander Serdyukov <alexander.serdyukov@gmail.com>
2025-10-05 13:15:42 -0600

    Problem 225

---
+++ problems/stacks_and_queues/problem_225.py
@@ -0,0 +1,105 @@
+"""
+LeetCode Problem 225: Implement Stack using Queues
+
+Implement a last-in-first-out (LIFO) stack using only two queues. The implemented stack should support all the functions of a normal stack (push, top, pop, and empty).
+
+Implement the MyStack class:
+- void push(int x) Pushes element x to the top of the stack.
+- int pop() Removes the element on the top of the stack and returns it.
+- int top() Returns the element on the top of the stack.
+- boolean empty() Returns true if the stack is empty, false otherwise.
+
+Notes:
+- You must use only standard operations of a queue, which means only push to back, peek/pop from front, size and is empty operations are valid.
+- Depending on your language, the queue may not be supported natively. You may simulate a queue using a list or deque (double-ended queue), as long as you use only the standard queue operations.
+
+Example 1:
+Input
+["MyStack", "push", "push", "top", "pop", "empty"]
+[[], [1], [2], [], [], []]
+Output
+[null, null, null, 2, 2, false]
+
+Explanation
+MyStack myStack = new MyStack();
+myStack.push(1);
+myStack.push(2);
+myStack.top(); // return 2
+myStack.pop(); // return 2
+myStack.empty(); // return False
+
+Constraints:
+- 1 <= x <= 9
+- At most 100 calls will be made to push, pop, top, and empty.
+- All the calls to pop and top are valid.
+
+Follow-up: Can you implement the stack using only one queue?
+"""
+
+from collections import deque
+
+
+class MyStack:
+    """
+    This task could be completed by using either expensive push, or expensive pop.
+    """
+
+    def __init__(self):
+        self.queue = deque()
+
+    def push(self, x: int) -> None:
+        self.queue.append(x)
+        for n in range(len(self.queue) - 1):
+            self.queue.append(self.queue.popleft())
+
+    def pop(self) -> int:
+        return self.queue.popleft()
+
+    def top(self) -> int:
+        return self.queue[0]
+
+    def empty(self) -> bool:
+        return not self.queue
+
+
+class MyStack1:
+
+    def __init__(self):
+        self.queue = deque()
+
+    def push(self, x: int) -> None:
+        aux = deque()
+        while self.queue:
+            aux.append(self.queue.popleft())
+        self.queue.append(x)
+        while aux:
+            self.queue.append(aux.popleft())
+
+    def pop(self) -> int:
+        return self.queue.popleft()
+
+    def top(self) -> int:
+        return self.queue[0]
+
+    def empty(self) -> bool:
+        return not self.queue
+
+
+import pytest
+
+
+def test_example_1():
+    """
+    Example 1:
+    Input
+    ["MyStack", "push", "push", "top", "pop", "empty"]
+    [[], [1], [2], [], [], []]
+    Output
+    [null, null, null, 2, 2, false]
+    """
+    myStack = MyStack()
+    myStack.push(1)
+    myStack.push(2)
+    assert myStack.top() == 2
+    assert myStack.pop() == 2
+    assert myStack.empty() == False
