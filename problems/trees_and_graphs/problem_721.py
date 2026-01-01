e757a93c5e82bb0a8e19a604c33e70a32ab2c1d4
Alexander Serdyukov <alexander.serdyukov@gmail.com>
2025-12-31 17:22:56 -0700

    Problem 721

--- problems/trees_and_graphs/problem_721.py
+++ problems/trees_and_graphs/problem_721.py
@@ -29,17 +29,98 @@ accounts[i][j] (for j > 0) is a valid email address.
 """
 
 from collections import defaultdict
-from typing import List
+from typing import List, Optional
 import pytest
 
 
 class Solution:
-    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
-        emails = defaultdict(list)
+    def accountsMerge(self, accounts: List[List[str]]) -> Optional[List[List[str]]]:
         graph = defaultdict(list)
+        email_to_name = {}
+
+        for account in accounts:
+            name = account[0]
+            first_email = account[1]
+            for email in account[1:]:
+                email_to_name[email] = name
+                graph[first_email].append(email)
+                graph[email].append(first_email)
+
+        visited = set()
+        merged_accounts = []
+
+        for email in graph:
+            if email in visited:
+                continue
+
+            visited.add(email)
+
+            component = [email]
+            stack = [email]
+
+            while stack:
+                node = stack.pop()
+                for neighbor in graph[node]:
+                    if neighbor not in visited:
+                        visited.add(neighbor)
+                        component.append(neighbor)
+                        stack.append(neighbor)
+
+            merged_accounts.append([email_to_name[email]] + sorted(component))
+
+        return merged_accounts
+
+
+class Solution1:
+    def accountsMerge(self, accounts: List[List[str]]) -> Optional[List[List[str]]]:
+        # Here I'll be keeping a list of indexes for each encountered email
+        emails: defaultdict[str, list[tuple[int, int]]] = defaultdict(list)
+        # Emails represent the nodes in the graph. This is adjecency list for this graph.
+        graph: defaultdict[tuple[int, int], list[tuple[int, int]]] = defaultdict(list)
+
         for i in range(len(accounts)):
             for j in range(1, len(accounts[i])):
-                emails[accounts[i][j]].append((i, j))
+                first_email_key = (i, 1)
+
+                this_email = accounts[i][j]
+                this_email_key = (i, j)
+
+                graph[this_email_key] = []
+                emails[this_email].append(this_email_key)
+
+                # Connect this occurence of email to all existing accounts where it is present
+                for key in emails[this_email]:
+                    if key != this_email_key:
+                        graph[this_email_key].append(key)
+                        graph[key].append(this_email_key)
+
+                if this_email_key != first_email_key:
+                    graph[first_email_key].append(this_email_key)
+                    graph[this_email_key].append(first_email_key)
+
+        seen: set[tuple[int, int]] = set()
+
+        def dfs(node, current_account):
+            current_account.add(accounts[node[0]][node[1]])
+            for neighbor in graph[node]:
+                if neighbor not in seen:
+                    seen.add(neighbor)
+                    dfs(neighbor, current_account)
+
+        answer = []
+
+        for email_key in graph:
+            if email_key in seen:
+                continue
+
+            account_name = accounts[email_key[0]][0]
+
+            current_account_emails = set()
+            dfs(email_key, current_account_emails)
+
+            answer.append([account_name] + list(sorted(current_account_emails)))
+
+        return answer
 
 
 @pytest.fixture
@@ -84,6 +165,37 @@ def test_002(sln):
         ["Kevin", "Kevin0@m.co", "Kevin3@m.co", "Kevin5@m.co"],
         ["Fern", "Fern0@m.co", "Fern1@m.co", "Fern5@m.co"],
     ]
+    # Sort both for comparison as order of accounts does not matter
+    result = sln.accountsMerge(accounts)
+    if result:
+        result.sort()
+        expected.sort()
+    assert result == expected
+
+
+def test_003(sln):
+    """Test the second example from the problem description."""
+
+    accounts = [
+        ["David", "David0@m.co", "David1@m.co"],
+        ["David", "David3@m.co", "David4@m.co"],
+        ["David", "David4@m.co", "David5@m.co"],
+        ["David", "David2@m.co", "David3@m.co"],
+        ["David", "David1@m.co", "David2@m.co"],
+    ]
+
+    expected = [
+        [
+            "David",
+            "David0@m.co",
+            "David1@m.co",
+            "David2@m.co",
+            "David3@m.co",
+            "David4@m.co",
+            "David5@m.co",
+        ]
+    ]
+
     # Sort both for comparison as order of accounts does not matter
     result = sln.accountsMerge(accounts)
     if result:
